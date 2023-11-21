9dbea12340d2e10723015ec1d0194cdcc797dbb0
xxchenxx <hfercxx@hotmail.com>
2023-11-21 16:06:01 +0000

    update

---
+++ merging.py
@@ -0,0 +1,17 @@
+import torch
+
+increment = 4
+
+interval_index = 1
+ipc = 2 * (interval_index + 1)
+all_data = []
+for i in range(25):
+    path = f"results/cifar100/conv3in_iterative_increase_grad_mse_nd2000_cut_niter2000_factor2_lr0.005_b_real128_mix_ipc2/interval_{interval_index}_data_{4*i}_{4*(i+1)}.pt"
+    data = torch.load(path)
+    all_data.append(data)
+
+prev = f"results/cifar100/conv3in_iterative_increase_grad_mse_nd2000_cut_niter2000_factor2_lr0.005_b_real128_mix_ipc2/interval_{interval_index}_data_{4*i}_{4*(i+1)}.pt"
+prev = torch.load(prev)
+
+for i in range(1, 25):
+    print((data[i] - prev).mean(1,2,3))
