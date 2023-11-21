d9208c35618fd9a10335599dbae9727f2ccec5e8
xxchenxx <hfercxx@hotmail.com>
2023-11-21 16:18:47 +0000

    update

--- merging.py
+++ merging.py
@@ -15,8 +15,11 @@ prev = torch.load(prev)
 print(prev)
 print(all_data[i][0])
 
+combined = [] 
 for i in range(25):
     print(i)
-    for j in range(100):
-        print(j)
-        print(prev[0][j*2+1] - all_data[i][0][j * 4+2])
+    combined.append(all_data[i][0][ipc * i : ipc * (i+1)])
+combined = torch.cat(combined, dim=0)
+label = all_data[0][1]
+
+torch.save([combined, label], f"results/cifar100/conv3in_iterative_increase_grad_mse_nd2000_cut_niter2000_factor2_lr0.005_b_real128_mix_ipc2/interval_{interval_index}_data.pt")
